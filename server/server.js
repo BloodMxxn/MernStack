68ed5edba54cde76ea09cb050b2ba30bf2c25db5
Blood Moon <163161573+BloodMxxn@users.noreply.github.com>
2025-02-11 14:38:50 +0330

    Delete server directory

--- server/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-const database = require("./utils/configs/database");
-
-database.connect();
-app.listen(process.env.PORT, () => {
-  console.log("> Server Running on port:", process.env.PORT);
-});
